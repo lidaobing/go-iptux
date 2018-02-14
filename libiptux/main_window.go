46c854bcacde754cbdcddd17f59cef0d12cc6a48
LI Daobing <lidaobing@gmail.com>
2018-02-14 13:28:30 +0800

    add aboutdialog

--- libiptux/main_window.go
+++ libiptux/main_window.go
@@ -15,9 +15,14 @@ func NewMainWindow() *MainWindow {
 
 	menubar := gtk.NewMenuBar()
 	box.Add(menubar)
+	menubar.Append(res.NewToolsMenu())
+	menubar.Append(res.NewHelpMenu())
 
+	return res
+}
+
+func (window *MainWindow) NewToolsMenu() gtk.IWidget {
 	menu := gtk.NewMenuItemWithMnemonic(T("_Tools"))
-	menubar.Append(menu)
 
 	submenu := gtk.NewMenu()
 	menu.SetSubmenu(submenu)
@@ -27,11 +32,32 @@ func NewMainWindow() *MainWindow {
 	//image := gtk.NewImageFromIconName(gtk.STOCK_ABOUT)
 	menuItem.SetImage(&image.Widget)
 	submenu.Append(menuItem)
-	menuItem.Connect("activate", res.onSharedManagement)
-	return res
+	menuItem.Connect("activate", window.onSharedManagement)
+	return menu
+}
+func (window *MainWindow) NewHelpMenu() gtk.IWidget {
+	menu := gtk.NewMenuItemWithMnemonic(T("_Help"))
+
+	submenu := gtk.NewMenu()
+	menu.SetSubmenu(submenu)
+
+	menuItem := gtk.NewImageMenuItemWithMnemonic(T("_About"))
+	image := gtk.NewImageFromIconName(gtk.STOCK_ABOUT, gtk.ICON_SIZE_MENU)
+	//image := gtk.NewImageFromIconName(gtk.STOCK_ABOUT)
+	menuItem.SetImage(&image.Widget)
+	submenu.Append(menuItem)
+	menuItem.Connect("activate", window.onAbout)
+	return menu
 }
 
 func (self *MainWindow) onSharedManagement() {
 	shareFile := NewShareFile(&self.Window)
 	shareFile.Run()
+	shareFile.Destroy()
+}
+
+func (self *MainWindow) onAbout() {
+	dialog := NewAboutDialog()
+	dialog.Run()
+	dialog.Destroy()
 }
